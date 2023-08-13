40364d0fa5b4279940e723668a57f5160d154b69
German <ponsger@Germans-MacBook-Pro.local>
2023-08-13 15:10:24 -0600

    Data static y componente que muestra opciones de busqueda

---
+++ src/data/citiesData.js
@@ -0,0 +1,74 @@
+export const citiesSearch = [
+    {
+      "Version": 1,
+      "Key": "1-1053393_1_AL",
+      "Type": "City",
+      "Rank": 85,
+      "LocalizedName": "Ecatepec",
+      "EnglishName": "San Pedro Ecatepec",
+      "PrimaryPostalCode": "",
+      "Region": {
+        "ID": "NAM",
+        "LocalizedName": "North America",
+        "EnglishName": "North America"
+      },
+      "Country": {
+        "ID": "MX",
+        "LocalizedName": "Mexico",
+        "EnglishName": "Mexico"
+      },
+      "AdministrativeArea": {
+        "ID": "TLA",
+        "LocalizedName": "Tlaxcala",
+        "EnglishName": "Tlaxcala",
+        "Level": 1,
+        "LocalizedType": "State",
+        "EnglishType": "State",
+        "CountryID": "MX"
+      },
+      "TimeZone": {
+        "Code": "CST",
+        "Name": "America/Mexico_City",
+        "GmtOffset": -6,
+        "IsDaylightSaving": false,
+        "NextOffsetChange": null
+      },
+      "GeoPosition": {
+        "Latitude": 19.522,
+        "Longitude": -98.155,
+        "Elevation": {
+          "Metric": {
+            "Value": 2643,
+            "Unit": "m",
+            "UnitType": 5
+          },
+          "Imperial": {
+            "Value": 8672,
+            "Unit": "ft",
+            "UnitType": 0
+          }
+        }
+      },
+      "IsAlias": true,
+      "ParentCity": {
+        "Key": "1053393",
+        "LocalizedName": "San Pedro Ecatepec",
+        "EnglishName": "San Pedro Ecatepec"
+      },
+      "SupplementalAdminAreas": [
+        {
+          "Level": 2,
+          "LocalizedName": "Atlangatepec",
+          "EnglishName": "Atlangatepec"
+        }
+      ],
+      "DataSets": [
+        "AirQualityCurrentConditions",
+        "AirQualityForecasts",
+        "Alerts",
+        "FutureRadar",
+        "MinuteCast",
+        "Radar"
+      ]
+    }
+  ]
