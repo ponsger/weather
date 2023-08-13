ffee3cc481bccc166907d9a363d45b6735a1c882
German <ponsger@Germans-MacBook-Pro.local>
2023-08-13 15:06:30 -0600

    Buscador offline funcionando con data estatica

--- src/components/SearchZone.jsx
+++ src/components/SearchZone.jsx
@@ -3,30 +3,37 @@ import '../css/searchzone/search.css'
 import '../css/searchzone/form.css'
 
 import { Fragment, useEffect, useState } from 'react';
+import OptionsInSearch from './OptionsInSearch';
 import { apiKey } from '../data/key'
 
 function SearchZone() {
-
     const [inputText, setInputText] = useState("");
     const [citiesFound, setCitiesFound] = useState([]);
 
-    const fetchCities = async () => {
-        const responseCitiesSearch = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${inputText}&offset=5`);
-        const citiesFound = await responseCitiesSearch.json();
-        return citiesFound;
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const handleChangeSearch = (e) => {
         setInputText(e.target.value);
-        const citiesFetched = await fetchCities();
-        if (citiesFetched)
-            setCitiesFound(...citiesFetched)
     }
 
-    const handleChangeCity = async (e) => {
 
-    }
+
+
+    // const fetchCities = async () => {
+    //     const responseCitiesSearch = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${inputText}&offset=5`);
+    //     const citiesFound = await responseCitiesSearch.json();
+    //     return citiesFound;
+    // }
+
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     setInputText(e.target.value);
+    //     const citiesFetched = await fetchCities();
+    //     if (citiesFetched)
+    //         setCitiesFound(...citiesFetched)
+    // }
+
+    // const handleChangeCity = async (e) => {
+
+    // }
 
 
     useEffect(() => {
@@ -35,12 +42,16 @@ function SearchZone() {
 
 
     return (
-        <div className="zone">
-            <form className='form-weather' onSubmit={(e) => handleSubmit(e)}>
-                <input className='search-weather' type='text' minLength={4} maxLength={120} />
-                <input className='search-button' type="submit" value="Search" />
-            </form>
-        </div>
+        <section>
+            <div className="zone">
+                <form className='form-weather' onSubmit={(e) => handleSubmit(e)}>
+                    <input className='search-weather' type='text' placeholder='Search city' minLength={4} maxLength={120} onChange={(event) => handleChangeSearch(event)} />
+                    <input className='search-button' type="submit" value="Search" />
+                </form>
+            </div>
+            <OptionsInSearch dataSearch={inputText} />
+        </section>
+
     );
 }
 
